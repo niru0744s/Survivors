3bb8ddf3882c1c979b09d91725f21b2d5b480cf3
niru0744s <nirupambhattacharya100@gmail.com>
2025-07-29 02:19:50 +0530

    deploy

---
+++ frontend/Public/svgs/MastercardIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+type SVGProps = React.SVGProps<SVGSVGElement>;
+const MastercardIcon = (props:SVGProps) => (
+  <svg width="41" height="25" viewBox="0 0 41 25" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M24.831 12.5C24.831 19.403 19.403 25 12.5 25C5.597 25 0.169 19.403 0.169 12.5C0.169 5.597 5.597 0 12.5 0C19.403 0 24.831 5.597 24.831 12.5Z"/>
+    <path d="M40.831 12.5C40.831 19.403 35.403 25 28.5 25C21.597 25 16.169 19.403 16.169 12.5C16.169 5.597 21.597 0 28.5 0C35.403 0 40.831 5.597 40.831 12.5Z"/>
+  </svg>
+);
+export default MastercardIcon;
---
+++ frontend/Public/svgs/PayPalIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+type SVGProps = React.SVGProps<SVGSVGElement>;
+const PayPalIcon = (props:SVGProps) => (
+  <svg width="80" height="26" viewBox="0 0 80 26" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M20.9 10.1C20.5 7.5 18.5 5.8 15.9 5.8H10.2L8.7 16.8H12.1L12.5 14.1H13C13.3 14.1 15.9 14.1 17.8 12.8C19.9 11.3 20.4 9.8 20.9 10.1ZM15.2 11.5H13.4L14.2 7.4H15.8C16.9 7.4 17.5 8 17.3 9.1C17.1 10.3 16.3 11.5 15.2 11.5Z"/>
+    <path d="M7.4 2.8H1.6L0 15.2H3.4L3.8 12.5H7.1C10 12.5 11.1 10.7 11.5 8.7C11.7 6.9 11 5.7 9.5 5.7H7.1L7.4 2.8ZM6.2 9.9H4.4L5 5.7H6.5C7.5 5.7 8.1 6.3 7.9 7.4C7.7 8.8 6.9 9.9 6.2 9.9Z" />
+  </svg>
+);
+export default PayPalIcon;
