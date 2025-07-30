084478e75c1b19fef768eb7aeb36857884fc6cd5
Harman Soni <harman.codes.dev@gmail.com>
2025-07-30 12:55:04 +0530

    Commit-Navbar

--- Survivors-next/boilerPlate/Navbar.tsx
+++ Survivors-next/boilerPlate/Navbar.tsx
@@ -1,63 +1,156 @@
-import React from 'react';
-import Link from 'next/link';
-// import 'tailwindcss'
-import Image from 'next/image';
-import './Navbar.css';
+'use client';
 
-const Navbar = () => {
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import ToolBar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { 
+    Box, 
+    Drawer, 
+    List, 
+    ListItem, 
+    ListItemButton, 
+    ListItemText, 
+    useMediaQuery 
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+// import Logo from '../Public/image/Logo_noBG.png';
+import CloseIcon from '@mui/icons-material/Close';
 
-    const Links = [
-        { name: "Home", link: "/" },
-        { name: "Services", link: "/services" },
-        { name: "About Us", link: "/about" },
-        { name: "Gaming", link: "/gaming" },
-        { name: "Blog", link: "/blog" },
-        { name: "Contact Us", link: "/contact" },
-        { name: "Login", link: "/login" }
-    ];
+const Navbar: React.FC = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
-    return (
-        <header className='bg-transparent text-white p-4 sticky-top'>
-            <nav className='container mx-auto flex justify-between items-center'>
-                <Link href="/" className='flex items-center'>
-                    <img src="image/Logo_noBG.png" alt="Logo" width={70} height={70} className='mr-2' />
-                    {/* <span className='text-xl font-bold'>Survivors</span> */}
-                </Link>
-                {/* <ul className='flex space-x-4 nav-links'>
-                    <li className='nav-item'>
-                        <Link href="/" className='nav-link'>Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/services" className='nav-link'>Services</Link>
-                    </li>
-                    <li>
-                        <Link href="/about" className='nav-link'>About Us</Link>
-                    </li>
-                    <li>
-                        <Link href="/gaming" className='nav-link'>Gaming</Link>
-                    </li>
-                    <li>
-                        <Link href={"/blog"} className='nav-link'>Blog</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact" className='nav-link'>Contact Us</Link>
-                    </li>
-                    <li>
-                        <Link href="/login" className='nav-link'>Login</Link>
-                    </li>
-                </ul> */}
-                <ul className='flex space-x-4 nav-links'>
-                    {Links.map((link, index) => (
-                        <li key={index} className='nav-item'>
-                            <Link href={link.link} className='nav-link'>
-                                {link.name}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    )
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+) => {
+    if (event.type === 'keydowm' && (
+        (event as React.KeyboardEvent).key === 'Tab' || 
+        (event as React.KeyboardEvent).key === 'shift'
+    )) {
+        return;
+    }
+
+    setDrawerOpen(open);
+  };
+
+  const Links = [
+    { name: 'Home', path: '/' },
+    { name: 'Services', path: '/services' },
+    { name: 'About us', path: '/aboutUs' },
+    { name: 'Gaming', path: '/gaming' },
+    { name: 'Blog', path: '/blog' },
+    { name: 'Contact us', path: '/contactUs' },
+    { name: 'Log in', path: '/login' }
+  ]
+
+  return (
+    <AppBar 
+    position='static'
+    >
+        <ToolBar>
+            <Box 
+            sx={{ flexGrow: 1 }}
+            >
+                <img 
+                src="image/Logo_noBG.png" 
+                alt='logo' 
+                style={{ height: 40 }} 
+                />
+            </Box>
+
+            {isMobile ? (
+                <>
+                    <IconButton
+                      edge='end'
+                      color='inherit'
+                      aria-label='menu'
+                      onClick={toggleDrawer(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Drawer
+                      anchor='right'
+                      open={drawerOpen}
+                      onClose={toggleDrawer(false)}
+                    >
+                        <Box
+                          sx={{ width: 200}}
+                          role='presentation'
+                          onClick={toggleDrawer(false)}
+                          onKeyDown={toggleDrawer(false)}
+                        >
+                            <Box
+                              sx={{
+                                display: 'flex',
+                                justifyContent: 'flex-end',
+                                p: 1
+                              }}
+                            >
+                                <IconButton
+                                  onClick={toggleDrawer(false)}
+                                >
+                                    <CloseIcon />
+                                </IconButton>
+                            </Box>
+                            <List>
+                                {Links.map((Link) => (
+                                    <ListItem
+                                      key={Link.name}
+                                      disablePadding
+                                    >
+                                        <ListItemButton
+                                          component='a'
+                                          href={Link.path}
+                                        >
+                                            <ListItemText 
+                                              primary={Link.name} 
+                                            />
+                                        </ListItemButton>
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Box>
+                    </Drawer>
+                </>
+            ) : (
+                <>
+                    <Box
+                      sx={{
+                        display: 'flex'
+                      }}
+                    >
+                        {Links.map((Link) => (
+                            <ListItem
+                              key={Link.name}
+                              disablePadding
+                              sx={{
+                                // display: 'inline-block',
+                                ml: 2
+                              }}
+                              className='nav-liks'
+                            >
+                                <ListItemButton
+                                  component='a'
+                                  href={Link.path}
+                                  className='nav-link'
+                                >
+                                    <ListItemText 
+                                      primary={Link.name}
+                                    />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </Box>
+                </>
+            )}
+        </ToolBar>
+    </AppBar>
+  )
 }
 
 export default Navbar;
